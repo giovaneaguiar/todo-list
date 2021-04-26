f195d290a204a899553109fad6ad1eeeb2c93e96
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-04-26 13:23:22 -0300

    Finaliza css de tarefa pendente e concluída.

--- tarefas/src/components/Task.vue
+++ tarefas/src/components/Task.vue
@@ -26,4 +26,37 @@ export default {
 };
 </script>
 <style>
+.task {
+    box-sizing: border-box;
+    width: 350px;
+    height: 150px;
+    padding: 10px;
+    border-radius: 8px;
+    font-size: 2rem;
+    font-weight: 300;
+    cursor: pointer;
+    user-select: none;
+    /* user select: o usuário não vai conseguir selecionar o texto */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+/* Estiliza quando tarefa pendente */
+.pending {
+    border-left: 12px solid #B73229;
+    background-color: #F44336;
+
+
+}
+/* Estiliza quando tarefa concluída */
+.done {
+    color:#ddd;
+    border-left: 12px solid #0A8F08;
+    background-color: #4CAF50;
+    text-decoration: line-through;
+    /* line-through: linha riscando o texto */
+}
+
 </style>
