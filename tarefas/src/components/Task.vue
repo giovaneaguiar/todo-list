ab4a173154bff8607c59b988421a857830b001ce
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-04-26 13:07:15 -0300

    Finaliza ideia de Task.vue com TaskGride.vue.

--- tarefas/src/components/Task.vue
+++ tarefas/src/components/Task.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="task">
+  <div class="task" :class="stateClass">
     <p>{{ task.name }}</p>
   </div>
 </template>
 
 <script>
 export default {
-    props: {
-        task: {
-            type: Object, required: true
-        }
+  props: {
+    task: {
+      type: Object,
+      required: true,
     },
-    computed: {
-        stateClass(){
-            //função para ajudar a saber se uma tarefa está pendente ou não.
-        }
-    }
+  },
+  computed: {
+    stateClass() {
+      //função para ajudar a saber se uma tarefa está pendente ou não.
+      return {
+        // se estiver pendente, marcar pendente
+        pending: this.task.pending,
+        //caso contrário, marcará classe css 'done'
+        done: !this.task.pending,
+      };
+    },
+  },
 };
 </script>
-
 <style>
 </style>
--- tarefas/src/components/TaskGrid.vue
+++ tarefas/src/components/TaskGrid.vue
@@ -1,17 +1,18 @@
 <template>
-<div class="task-grid">
-
+  <div class="task-grid">
     <template>
-        <!-- faz um for para mostrar dentro de cada uma das divs o nome das tasks -->
-        <!-- :key="task.name" serve para não aceitar duas tasks com o mesmo nome -->
-        <div v-for="task in tasks" :key="task.name">{{ task.name }}</div>
+      <!-- faz um for para mostrar dentro de cada uma das divs o nome das tasks -->
+      <!-- :key="task.name" serve para não aceitar duas tasks com o mesmo nome -->
+      <!-- :task="task" serve para passar o valor como um objeto, e não como string -->
+      <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
     </template>
-    
-</div>
+  </div>
 </template>
 
 <script>
+import Task from "./Task.vue";
 export default {
+  components: { Task },
   //Componente que precisa receber uma lista de tarefas.
   // required: true (significa um atributo obrigatório).
   props: {
@@ -22,6 +23,5 @@ export default {
   },
 };
 </script>
-
 <style>
 </style>
