3b3ec02f36aeabae96f0c9e1b1f71709cd57b60d
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-04-26 18:09:50 -0300

    Estiliza input e button.

--- tarefas/src/components/NewTask.vue
+++ tarefas/src/components/NewTask.vue
@@ -3,8 +3,14 @@
     <!-- Formulário para adicionar uma nova tarefa,
       juntamente com um botão -->
 
-    <input v-model="name" @keydown.enter="add" type="text">
-    <button @click="add">+</button>
+    <input
+      class="form-element"
+      placeholder="Nova tarefa?"
+      v-model="name"
+      @keydown.enter="add"
+      type="text"
+    />
+    <button class="form-element" @click="add">+</button>
     <!-- Tanto o click do botão, quanto o enter, chamam o método add() -->
   </div>
 </template>
@@ -22,12 +28,39 @@ export default {
             enter ou o botão '+', mandar para o componente pai (App.vue) de que uma task foi adicionada.
             E também limpar o valor de 'name' para incluir outra tarefa, por exemplo.
             */
-           this.$emit('taskAdded', {name: this.name});
-           this.name = '';
+      this.$emit("taskAdded", { name: this.name });
+      this.name = "";
     },
   },
 };
 </script>
-
 <style>
+.new-task {
+  margin: 35px;
+}
+
+.form-element {
+    outline: none;
+    font-size: 2rem;
+    border: 1px solid #fff;
+    padding: 5px 10px 8px;
+    color: #fff;
+
+}
+
+input.form-element {
+    width: 500px;
+    background-color: #fff2;
+    /* bordas apenas no lado esquerdo */
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+
+}
+
+button.form-element{
+    border-left: none;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    background-color: #AB82FF;
+}
 </style>
