f7d81b341c8e71887a6db744ffacde6c0c8ae020
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-04-26 00:54:36 -0300

    inicializa projeto TODO com título e body css.

---
+++ tarefas/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- Título -->
+    <h1>Tarefas</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* Estiliza o fundo da aplicação 
+   Utiliza degradê com o linear-gradient
+*/
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(71, 60, 139), rgb(72, 118, 255));
+  color: #fff;
+}
+
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
+</style>
