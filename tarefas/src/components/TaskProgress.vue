33bd1f3bd5930372bf0bbc409b2482ea0be40dad
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-04-27 12:54:29 -0300

    Cria component TaskProgress.

---
+++ tarefas/src/components/TaskProgress.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tasks-progress">
+      <span class="progress-value">
+          {{ progress }}%
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
