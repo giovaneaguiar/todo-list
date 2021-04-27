e751a708d96de32fcf94b43cc88b5b335078ec02
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-04-27 13:06:49 -0300

    Finaliza parte lógica do TaskProgress.

--- tarefas/src/App.vue
+++ tarefas/src/App.vue
@@ -2,22 +2,37 @@
   <div id="app">
     <!-- Título -->
     <h1>Tarefas</h1>
+    <TaskProgress :progress="progress"></TaskProgress>
     <!-- @taskAdded="addTask" serve para monitorar quando o evento acontecer,
     chamar o método -->
     <New-Task @taskAdded="addTask"></New-Task>
-    <TaskGrid @taskDeleted="deleteTask"
+    <TaskGrid
+      @taskDeleted="deleteTask"
       @taskStateChanged="toggleState"
-     :tasks="tasks"></TaskGrid>
-    
+      :tasks="tasks"
+    ></TaskGrid>
   </div>
 </template>
 
 <script>
 import TaskGrid from "./components/TaskGrid.vue";
 import NewTask from "./components/NewTask";
+import TaskProgress from "./components/TaskProgress";
 export default {
   components: {
-    TaskGrid, NewTask
+    TaskGrid,
+    NewTask,
+    TaskProgress,
+  },
+  computed: {
+    progress() {
+      const total = this.tasks.length;
+      // total: pega o tamanho do array.
+      const done = this.tasks.filter((t) => !t.pending).length;
+      // done: função que pega todos que não estão pendentes.
+      return Math.round((done / total) * 100) || 0;
+      // retorna calculo correto para fazer a barra de porcentagem.
+    },
   },
   data() {
     return {
@@ -26,34 +41,29 @@ export default {
       ],
     };
   },
-  
-  methods: {
-    
-    addTask(task){
-      // Cria consts para saber se a task que vai ser adicionada existe ou não na lista.
-      const sameName = t => t.name === task.name
-      const reallyNew = this.tasks.filter(sameName).length == 0
-      if(reallyNew) {
-        // Se for realmente novo, inclui no array, se não, ignora.
-        this.tasks.push({name: task.name,
-        pending: task.pending || true})
 
+  methods: {
+    addTask(task) {
+      // Cria consts para saber se a task que vai ser adicionada existe ou não na lista.
+      const sameName = (t) => t.name === task.name;
+      const reallyNew = this.tasks.filter(sameName).length == 0;
+      if (reallyNew) {
+        // Se for realmente novo, inclui no array, se não, ignora.
+        this.tasks.push({ name: task.name, pending: task.pending || true });
       }
-      
     },
-    deleteTask(i){
+    deleteTask(i) {
       this.tasks.splice(i, 1);
       // i: indice do array que quero deletar, ou seja, qual task eu quero
       // 1: numero de tasks que quero deletar, ou seja, 1 task apenas.
     },
 
-    toggleState(i){
+    toggleState(i) {
       // se estiver pendente, ele coloca falso
       // se não estiver pendente, ele coloca verdadeiro
-      this.tasks[i]. pending = !this.tasks[i].pending 
-    }
-  }
-
+      this.tasks[i].pending = !this.tasks[i].pending;
+    },
+  },
 };
 </script>
 
--- tarefas/src/components/TaskProgress.vue
+++ tarefas/src/components/TaskProgress.vue
@@ -3,12 +3,16 @@
       <span class="progress-value">
           {{ progress }}%
       </span>
+      <div class="progress-bar" :style="{width: progress + '%'}"></div>
   </div>
 </template>
 
 <script>
 export default {
-
+        
+        props:{
+            progress: { type: Number, default: 0 } 
+        }
 }
 </script>
 
