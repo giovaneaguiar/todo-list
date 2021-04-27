e6aa91e8680d4fb97bbc5463067fc40ceb0de298
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-04-27 12:11:58 -0300

    Usa @click.stop para corrigir "erro" de chamadas.

--- tarefas/src/components/Task.vue
+++ tarefas/src/components/Task.vue
@@ -2,7 +2,7 @@
   <div 
      @click="$emit('taskStateChanged', task)"
      class="task" :class="stateClass">
-    <span @click="$emit('taskDeleted', task)" class="close">x</span>
+    <span @click.stop="$emit('taskDeleted', task)" class="close">x</span>
     <p>{{ task.name }}</p>
   </div>
 </template>
